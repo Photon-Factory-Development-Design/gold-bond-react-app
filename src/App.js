1e44119a2a913e05e4850535f23b62aa1bc1f2b0
TalentWebDev <49565243+talentwebdev@users.noreply.github.com>
2021-03-03 10:49:25 +0100

    add material ui and header.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-const logo_url = "https://cdn.jsdelivr.net/gh/Photon-Factory-Development-Design/gold-bond-react-app/src/logo.svg"
+import { Box } from '@material-ui/core';
+import { Header } from './components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo_url} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+        <Header />
+    </Box>
   );
 }
 
---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Box } from '@material-ui/core';
+
+const headerBackImage = 'https://cdn.jsdelivr.net/gh/Photon-Factory-Development-Design/gold-bond-react-app/src/assets/images/Header.jpg';
+const Header = () => (
+    <Box style={{ backgroundImage: headerBackImage, color: '#f00' }}>1123</Box>
+);
+
+export default Header;
