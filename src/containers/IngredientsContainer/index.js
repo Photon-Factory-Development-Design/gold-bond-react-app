3467b35e0dc5a869f9fdda10e98b57c408d9e9e1
TalentWebDev <49565243+talentwebdev@users.noreply.github.com>
2021-03-06 03:43:27 +0800

    upgrade ingredients container.

--- src/containers/IngredientsContainer/index.js
+++ src/containers/IngredientsContainer/index.js
@@ -1,8 +1,30 @@
-import { Container, Box } from '@material-ui/core';
 import React from 'react';
+import { Container, Box } from '@material-ui/core';
 import { BackgroundContainer, Typography } from '../../components';
+import ingredients from './ingredients';
 
-const QuizContainer = () => {
+const IngredientItem = ({ url, label }) => (
+    <Box m={2}>
+        <BackgroundContainer color="secondary">
+            <Box
+                width={150}
+                height={170}
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+                alignItems="center"
+                p={2}>
+                <img src={url} height="50" alt="ingredient" />
+                <Box py={2}>
+                    <Typography variant="body1" align="center" color="primary">
+                        <Box fontWeight={900}>{label}</Box>
+                    </Typography>
+                </Box>
+            </Box>
+        </BackgroundContainer>
+    </Box>
+);
+const IngrdientsContainer = () => {
     return (
         <BackgroundContainer color="primary" vertical="8">
             <Box py={2}>
@@ -13,13 +35,29 @@ const QuizContainer = () => {
 
             <Container maxWidth="md">
                 <Typography color="secondary" variant="body1" align="center">
-                    We use the very best, research-backed ingredients chosen by dermatologists. 
-                    But far more importantly, we use them in just the right formula for you. 
-                    You need to use just the right ones at the amount that is safe and effective for your skin.
+                    We use the very best, research-backed ingredients chosen by
+                    dermatologists. But far more importantly, we use them in
+                    just the right formula for you. You need to use just the
+                    right ones at the amount that is safe and effective for your
+                    skin.
                 </Typography>
+
+                <Box
+                    display="flex"
+                    flexDirection="row"
+                    width="100%"
+                    flexWrap="wrap"
+                    mt={2}>
+                    {ingredients.map((ingredient, index) => (
+                        <IngredientItem
+                            key={`ingredient-item-${index}`}
+                            {...ingredient}
+                        />
+                    ))}
+                </Box>
             </Container>
         </BackgroundContainer>
-    )
+    );
 };
 
-export default QuizContainer;
+export default IngrdientsContainer;
